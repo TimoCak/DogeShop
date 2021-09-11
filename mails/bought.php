7209c80da9d184c39ef7f0d55e9ff0ae8cfb5276
timo.caktu@student.uni-tuebingen.de <timocaktu@gmail.com>
2021-09-12 01:29:13 +0200

    changed the mail account, need to think about the buy procedure now!

--- mails/bought.php
+++ mails/bought.php
@@ -3,6 +3,7 @@
 include "../PHPMailer/src/PHPMailer.php";
 include "../PHPMailer/src/SMTP.php";
 include "../PHPMailer/src/Exception.php";
+include "../includes/functions.inc.php";
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
@@ -10,6 +11,9 @@ use PHPMailer\PHPMailer\Exception;
 
 
 $mail = new PHPMailer(true);
+$seller = $_POST["seller"];
+$buyer = $_POST["buyer"];
+$pid = $_POST["pid"];
 
 try {
     //Server settings
@@ -17,14 +21,14 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'timocaktu@gmail.com';                     //SMTP username
-    $mail->Password   = 'CaktuTimo96';                               //SMTP password
+    $mail->Username   = 'dogeshop8@gmail.com';                     //SMTP username
+    $mail->Password   = 't7i9m8o12';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;               //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('no-reply@example.org', 'Mailer');
-    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+    $mail->setFrom('no-reply@dogeshop.xyz', 'Doge Shop');
+    $mail->addAddress('timocaktu@gmail.com', 'Timo Caktu');     //Add a recipient
     #$mail->addAddress('ellen@example.com');               //Name is optional
     #$mail->addReplyTo('info@example.com', 'Information');
     #$mail->addCC('cc@example.com');
@@ -36,7 +40,7 @@ try {
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
+    $mail->Subject = 'You bought ';
     $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
